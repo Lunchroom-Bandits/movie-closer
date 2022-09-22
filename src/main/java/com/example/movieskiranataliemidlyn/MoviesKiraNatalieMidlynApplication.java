28554fe9434a95c5bb08497930a60d4e8cb7463b
midlynj <jmidlyn@gmail.com>
2022-09-22 11:05:59 -0500

    pomfile added

---
+++ src/main/java/com/example/movieskiranataliemidlyn/MoviesKiraNatalieMidlynApplication.java
@@ -0,0 +1,13 @@
+package com.example.movieskiranataliemidlyn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesKiraNatalieMidlynApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoviesKiraNatalieMidlynApplication.class, args);
+    }
+
+}

2b1421f61d60144694b6a1b228e254dd187e130e
midlynj <jmidlyn@gmail.com>
2022-09-22 14:54:20 -0500

    able to add a movie method created

--- src/main/java/com/example/movieskiranataliemidlyn/Controllers/MoviesController.java
+++ src/main/java/com/example/movieskiranataliemidlyn/Controllers/MoviesController.java
@@ -2,9 +2,7 @@ package com.example.movieskiranataliemidlyn.Controllers;
 
 import com.example.movieskiranataliemidlyn.Class.Movies;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,11 +11,23 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/api/movies", produces = "application/json")
 public class MoviesController {
-
+    List<Movies> movies =  new ArrayList<>();
     @GetMapping("")
         private List<Movies> allMovies() {
-            List<Movies> movies =  new ArrayList<>();
+
             movies.add(new Movies(1L,"meep","comedy","pg13","Midlyn"));
             return movies;
     }
+
+    @PostMapping("")
+        private void addAMovie(@RequestBody Movies newMovie) {
+           movies.add(newMovie);
+    }
+
+    @DeleteMapping("/{id}")
+        private void deleteMovie(@PathVariable Long id) {
+        movies.remove(id);
+    }
+
+
 }

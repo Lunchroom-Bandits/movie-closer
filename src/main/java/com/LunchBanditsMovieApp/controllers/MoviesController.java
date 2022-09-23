9fb9576270a3b5458662b334f559d7ca8d2fe9be
NatalieSneed <106278703+NatalieSneed@users.noreply.github.com>
2022-09-23 09:41:16 -0500

    updates made to MoviesController

--- src/main/java/com/LunchBanditsMovieApp/controllers/MoviesController.java
+++ src/main/java/com/LunchBanditsMovieApp/controllers/MoviesController.java
@@ -1,32 +1,63 @@
 package com.LunchBanditsMovieApp.controllers;
 
+import com.LunchBanditsMovieApp.data.Movies;
+import com.LunchBanditsMovieApp.repositories.MoviesRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.BeanUtils;
+import org.springframework.data.annotation.Id;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping(value = "/api/movies", produces = "application/json")
 public class MoviesController {
-    List<Movies> movies =  new ArrayList<>();
-    @GetMapping("")
-        private List<Movies> allMovies() {
+    private MoviesRepository moviesRepository;
 
-            movies.add(new Movies(1L,"meep","comedy","pg13","Midlyn"));
+
+    @GetMapping("")
+        public List<Movies> allMovies() {
+        List <Movies> movies=moviesRepository.findAll();
+//            movies.add(new Movies(1L,"meep","comedy","pg13","Midlyn"));
             return movies;
+    }
+    @GetMapping("/{id}")
+    public Optional<Movies> fetchMoviesById(@PathVariable long id) {
+        Optional<Movies> optionalMovies = moviesRepository.findById(id);
+        if(optionalMovies.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Post id " + id + "not found ");
+        }
+        return optionalMovies;
     }
 
     @PostMapping("")
         private void addAMovie(@RequestBody Movies newMovie) {
-           movies.add(newMovie);
+           moviesRepository.save(newMovie);
     }
 
     @DeleteMapping("/{id}")
         private void deleteMovie(@PathVariable Long id) {
-        movies.remove(id);
+        moviesRepository.deleteById(id);
     }
+    @PutMapping("/{id}")
+    public void updateMovies(@RequestBody Movies updatedMovies, @PathVariable long id) {
+        Optional<Movies> optionalMovies = moviesRepository.findById(id);
+        if (optionalMovies.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Post " + id + " not found");
+        }
+        Movies originalPost = optionalMovies.get();
+
+        updatedMovies.setId(id);
+        moviesRepository.save(updatedMovies);
+    }
+    // copy any new field values FROM updatedPost TO originalPost
+//        BeanUtils.copyProperties(updatedPost, originalPost, FieldHelper.getNullPropertyNames(updatedPost));
 
 
 }
+
